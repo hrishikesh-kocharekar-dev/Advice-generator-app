c240e1e7afdd2722c6acd4ca2a419928a1290799
Hrishikesh <hrishikesh.sde@gmail.com>
2023-03-21 23:57:34 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const fetchAdvice = async() => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json()
+
+    document.getElementById('title').innerHTML = `Advice #${data.slip.id}`;
+    document.getElementById('text').innerHTML = `"${data.slip.advice}"`;
+}
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    fetchAdvice();
+})
+
+
